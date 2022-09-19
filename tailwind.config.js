68786425d19360be0dcc826990081cec85db34b7
dhia13 <dhiachabane@yahoo.fr>
2022-09-19 03:01:23 +0200

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'Hahmlet': ['"Hahmlet"', 'serif'],
-        'poppins': ['"Poppins"', 'sans-serif'],
-      },
-      keyframes: {
-        flip: {
-          "50%,80%": {
-            transform: "rotateY(360deg)"
-          }
-        }
-      },
-      animation: {
-        "flip": 'flip 2s infinite'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-module.exports = {
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./public/**/*.html",
-  ],
-  plugins: [],
-  theme: {},
-};
