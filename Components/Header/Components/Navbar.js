2967342948f6829b34329d8febb337770359aa1a
dhia13 <dhiachabane@yahoo.fr>
2022-12-06 19:17:14 +0100

    change navbar color

--- Components/Header/Components/Navbar.js
+++ Components/Header/Components/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar({ setCurrent, current }) {
     return (
         <div className='w-full h-full'>
-            <div className='w-full h-full flex justify-between items-center'>
+            <div className='w-full h-full flex justify-between items-center bg-black'>
                 {/* Logo */}
                 <div className='flex justify-center items-center px-2 ml-2 h-full z-50'>
                     <button onClick={() => setCurrent('header')}>
