c4b837f3158b2923a0b1f62d3906be85d2dc3f06
sahhar dhia eddine <dhiachabane@yhaoo.fr>
2022-12-10 14:48:03 -0800

    fix package

--- Components/Background/TriangleBg/TriangleBg.js
+++ Components/Background/TriangleBg/TriangleBg.js
@@ -1,14 +1,11 @@
-import { useEffect } from "react"
-
 const TriangleBg = () => {
-
   return (
-    <div className='block absolute top-0 z-40'>
+    <div className="block absolute top-0 z-40">
       <div id="container" className="absolute h-full w-full">
         <div id="output" className="absolute h-full w-full"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TriangleBg
+export default TriangleBg;
