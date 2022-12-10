63f0cc200ff5aec61cb6341b75105d1f86e383bf
sahhar dhia eddine <dhiachabane@yhaoo.fr>
2022-12-10 11:52:58 -1200

    adding social media link

--- Components/ContactFloters.js
+++ Components/ContactFloters.js
@@ -1,29 +1,106 @@
-import Link from "next/link"
+import { useState } from "react";
 
 const ContactFloters = () => {
-    return (
-        <div className='absolute z-50 w-[50px] top-1/3 right-0 opacity-70 hover:opacity-100 bg-black rounded-tl-md rounded-bl-md cursor-pointer flex flex-col justify-center items-center'>
-            <div className='flex justify-center items-center h-[50px] w-[50px]'>
-                <a href='https://www.linkedin.com/in/sahhar-dhia-eddine-b9a45824b/' target="_blank">
-                    <img src='/images/linkedin.png' alt='linkedin' className='w-[28px] h-[28px] hover:w-[36px] hover:h-[36px]' />
-                </a>
-            </div>
-            <div className='flex justify-center items-center h-[50px] w-[50px]'>
-                <a href='https://github.com/dhia13' target="_blank">
-                    <img src='/images/github.png' alt='github' className='w-[28px] h-[28px] hover:w-[36px] hover:h-[36px]' />
-                </a>
-            </div>
-            <div className='flex justify-center items-center h-[50px] w-[50px]'>
-                <a href='https://t.me/+213794064884' target="_blank">
-                    <img src='/images/telegramContact.png' alt='github' className='w-[28px] h-[28px] hover:w-[36px] hover:h-[36px]' />
-                </a>
-            </div>
-            <div className='flex justify-center items-center h-[50px] w-[50px]'>
-                <a href='http://m.me/sahhar13' target="_blank">
-                    <img src='/images/facebookContact.png' alt='github' className='w-[28px] h-[28px] hover:w-[36px] hover:h-[36px]' />
-                </a>
-            </div>
-        </div>)
-}
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      onMouseMove={() => setOpen(true)}
+      onMouseOut={() => setOpen(false)}
+      className={`absolute z-50 w-[50px] top-1/3 right-0 ${
+        !open && "opacity-50 w-[50px]"
+      }  ${
+        open && "opacity-100 w-[170px]"
+      } bg-black rounded-tl-md rounded-bl-md cursor-pointer flex flex-col
+       justify-start items-center`}
+    >
+      <a
+        className={`flex justify-start items-center gap-4 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="https://github.com/dhia13"
+        target="_blank"
+      >
+        <img
+          src="/images/github.png"
+          alt="Github"
+          className="w-[28px] h-[28px]"
+        />
+        {open && <p className="text-white font-pressStart text-xs">Github</p>}
+      </a>
+      <a
+        className={`flex justify-start items-center gap-2 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="https://www.linkedin.com/in/sahhar-dhia-eddine-b9a45824b/"
+        target="_blank"
+      >
+        <img
+          src="/images/linkedin.png"
+          alt="linkedin"
+          className="w-[28px] h-[28px]"
+        />
+        {open && <p className="text-white font-pressStart text-xs">Linkedin</p>}
+      </a>
+      <a
+        className={`flex justify-start items-center gap-2 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="https://t.me/+213794064884"
+        target="_blank"
+      >
+        <img
+          src="/images/telegramContact.png"
+          alt="telegram"
+          className="w-[28px] h-[28px]"
+        />
+        {open && <p className="text-white font-pressStart text-xs">Telegram</p>}
+      </a>
+      <a
+        className={`flex justify-start items-center gap-2 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="https://www.codewars.com/users/dhia13"
+        target="_blank"
+      >
+        <img
+          src="/images/codewars.png"
+          alt="CodeWars"
+          className="w-[28px] h-[28px]"
+        />
+        {open && <p className="text-white font-pressStart text-xs">CodeWars</p>}
+      </a>
+      <a
+        className={`flex justify-start items-center gap-2 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="http://m.me/sahhar13"
+        target="_blank"
+      >
+        <img
+          src="/images/facebookContact.png"
+          alt="Messenger"
+          className="w-[28px] h-[28px]"
+        />
+        {open && (
+          <p className="text-white font-pressStart text-xs">Messenger</p>
+        )}
+      </a>
+      <a
+        className={`flex justify-start items-center gap-2 ${
+          open && "w-[150px]"
+        } h-[50px]`}
+        href="https://www.codewars.com/users/dhia13"
+        target="_blank"
+      >
+        <img
+          src="/images/email.png"
+          alt="email"
+          className="w-[28px] h-[28px]"
+        />
+        {open && <p className="text-white font-pressStart text-xs">Email</p>}
+      </a>
+    </div>
+  );
+};
 
-export default ContactFloters
+export default ContactFloters;
