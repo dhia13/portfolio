2abb5eb850f0d72b6387203d7a53bac82c22636a
sahhar dhia eddine <dhiachabane@yhaoo.fr>
2022-12-14 21:47:31 +0100

    adding title in head

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&family=Press+Start+2P&family=Roboto:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <title>Kizaru</title>
         </Head>
         <body>
           <Main />
